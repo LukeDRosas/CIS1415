e0e5b4548fedbf22e66942873ec086147517d36c
LukeDRosas <33234746+LukeDRosas@users.noreply.github.com>
2017-10-30 18:13:33 -0500

    Add files via upload

---
+++ chp7.extra.initials.py
@@ -0,0 +1,12 @@
+#Luke Dought-ROsas
+
+firstName = input('Enter First Name:')
+middleName = input('Enter Middle Name:')
+lastName = input('Enter Last Name:')
+
+
+firstName = firstName[0]
+middleName = middleName[0]
+lastName = lastName[0]
+
+print('%s. %s. %s.' % (firstName, middleName, lastName))
