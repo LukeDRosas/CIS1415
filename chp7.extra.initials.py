26fab6476af7fd05caea5b15a44b491f0ac30ba6
LukeDRosas <33234746+LukeDRosas@users.noreply.github.com>
2017-10-30 19:09:59 -0500

    Add files via upload

--- chp7.extra.initials.py
+++ chp7.extra.initials.py
@@ -1,12 +1,12 @@
 #Luke Dought-ROsas
 
-firstName = input('Enter First Name:')
-middleName = input('Enter Middle Name:')
-lastName = input('Enter Last Name:')
+fullName = input('Enter your full name:')
+initials = []
+count = 0
 
+fullName = fullName.split()
+for name in fullName:
+    name = name[0]
+    initials.append(name)
 
-firstName = firstName[0]
-middleName = middleName[0]
-lastName = lastName[0]
-
-print('%s. %s. %s.' % (firstName, middleName, lastName))
+print('%s. %s. %s.' % (initials[0], initials[1], initials[2]))
